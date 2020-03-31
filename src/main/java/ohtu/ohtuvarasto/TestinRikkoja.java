438b19d3cc5c5b515c94a2b0d8ce6017c93f04e7
toppyy <topias.pyykkonen@helsinki.fi>
2020-03-31 20:53:04 +0300

    viikko 2 - tehtava 2

---
+++ src/main/java/ohtu/ohtuvarasto/TestinRikkoja.java
@@ -0,0 +1,37 @@
+
+package ohtu.ohtuvarasto;
+
+public class TestinRikkoja {
+
+
+
+    /*
+    public void liianPitkaMetodi(int input) {
+      
+        if (1+input==2) {
+            if (3*input==6) {
+                
+                for (int i = 0; i < 10;i++) 
+                {
+                    for (int z = 0; z < 10;z++)  {
+                System.out.println("....");
+                    }
+                }
+            } else {
+                if (input>0) {
+                    if (input<10)
+                {
+                        System.out.println("...");
+                    } else {
+                        System.out.println("Madness");
+                    }
+                } 
+            }
+        }
+       
+    }
+
+    */
+
+
+}
