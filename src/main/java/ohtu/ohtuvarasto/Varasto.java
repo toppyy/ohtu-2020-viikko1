ab417e0130511c6d1ad29a0992cbe6e2925e3fd9
toppyy <topias.pyykkonen@helsinki.fi>
2020-04-03 20:34:07 +0300

    code style violation

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -30,7 +30,8 @@ public class Varasto {
     }
 
     // --- ottavat aksessorit eli getterit: ---
-    public double getSaldo() {
+    public double getSaldo()
+    {
         return saldo;
     }
 
